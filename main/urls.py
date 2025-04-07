391e97e27968053d9e961fcf0093ddd311a8e84c
Mihail <mirniymichail@yandex.ru>
2025-04-07 21:41:06 +0300

    Step626

--- main/urls.py
+++ main/urls.py
@@ -9,6 +9,7 @@ from .views import ChangeUserInfoView
 from .views import BBPasswordChangeView
 from .views import RegisterUserView, RegisterDoneView
 from .views import user_activate
+from .views import DeleteUserView
 
 
 app_name = 'main'
@@ -22,10 +23,13 @@ urlpatterns = [
     path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
     path('accounts/password/change/', BBPasswordChangeView.as_view(),
                                       name='password_change'),
+    path('accounts/profile/delete/', DeleteUserView.as_view(),
+                                     name='delete_user'),
+
     path('accounts/profile/change/', ChangeUserInfoView.as_view(),
                                      name='profile_change'),
     path('accounts/profile/', profile, name='profile'),
     path('accounts/login/', BBLoginView.as_view(), name='login'),
     path('<str:page>/', other_page, name='other'),
-    path('', index, name='index'),
+    path('', index, name='index')
 ]
